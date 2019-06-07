48dff83c091c4fae8f0d6fe4155872d31e0f7f4e
Sander Janssens <sander-ja@hotmail.com>
2019-06-07 09:37:05 +0200

    update2

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,26 +1,40 @@
 package be.vdab.luigi.controllers;
 
+import be.vdab.luigi.domain.Adres;
+import be.vdab.luigi.domain.Persoon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Controller
 @RequestMapping("/")
 class IndexController {
-    @GetMapping
-    public ModelAndView index() {
+    private String boodschap() {
         int uur = LocalTime.now().getHour();
         if (uur < 12) {
-            return new ModelAndView("index", "boodschap", "morgen");
+            return "morgen";
         }
         if (uur < 18) {
-            return new ModelAndView("index", "boodschap", "middag");
+            return "middag";
         }
-        return new ModelAndView("index", "boodschap", "avond");
+        return "avond";
     }
+    @GetMapping
+    public ModelAndView index() {
+
+            ModelAndView modelAndView = new ModelAndView("index", "boodschap", "morgeuh");
+            modelAndView.addObject("zaakvoerder", new Persoon("Sander", "Janssens",
+                    0, false, LocalDate.of(1992,7,9), new Adres("Oversneslaan", "47", 2610, "Wilrijk")));
+            return modelAndView;
+
+    }
+
+
 
 
 }
